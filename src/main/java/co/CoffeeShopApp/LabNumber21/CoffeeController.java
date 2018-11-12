4ad61754acc537bfd816d0816526b0cbf3dc5d3e
Acastalope <acastalope@gmail.com>
2018-11-12 16:54:35 -0500

    initial commit

---
+++ src/main/java/co/CoffeeShopApp/LabNumber21/CoffeeController.java
@@ -0,0 +1,14 @@
+package co.CoffeeShopApp.LabNumber21;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class CoffeeController {
+	
+	@RequestMapping("/")
+	public ModelAndView myIndex() {
+
+	return myIndex();
+}	
+}
